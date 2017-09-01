13b02e54d03416372780ee08a1d4db8e7027350f
Tiffany Tarab <tiffany.tarab@gmail.com>
2017-09-01 14:48:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday, name) {
-  return "Happy ${holiday}, ${name}!"
+  return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(holiday, days) {
-  return "It's ${days} days until ${holiday}!"
-}
+function holidayCountdown(holiday, days)
